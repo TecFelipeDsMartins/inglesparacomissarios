2ab76c331fcd008733ac98078e80e255d2f5f611
Felipe Martins <tec.felipedsmartins@gmail.com>
2021-09-21 18:01:18 -0300

    finished layout home - responsive

--- components/CardMenu.js
+++ components/CardMenu.js
@@ -1,11 +1,11 @@
 const Card = (props) => {
   return(
-    <div class="flex flex-col md:w-3/12 md:h-3/5 m-8 p-3 space-y-5 rounded-xl   bg-white shadow-lg " 
+    <div className="flex flex-col sm:w-1/3  sm:h-4/6 w-auto h-40        m-8 p-3 space-y-5 rounded-xl   bg-white shadow-lg " 
         style={{backgroundImage: props.img,
                 backgroundSize: 'cover',
                 backgroundPosition: 'center'}}>
-        <section class="h-screen flex justify-center items-end">
-            <button type="button" class=" w-full bg-gray-100 text-blue-900 hover:bg-gray-200 px-3 py-1 rounded-md text-1xl">{props.name}</button>
+        <section className="h-screen flex justify-center items-end">
+            <button type="button" className=" w-full bg-gray-100 text-blue-900 hover:bg-gray-200 px-3 py-1 rounded-md text-1xl">{props.name}</button>
         </section>
     </div>
   )
