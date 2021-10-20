d8ee7f80a1cd156f2c6290cb1b2e6734b9a42cb4
Felipe Martins <tec.felipedsmartins@gmail.com>
2021-10-19 21:46:46 -0300

    added trash icon

--- components/Mic.js
+++ components/Mic.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 import { FaMicrophone } from 'react-icons/fa';
+import { GiTrashCan } from 'react-icons/gi';
 
 const Mic = () => {
   const [ speechText, setSpeechText ] = useState()
@@ -16,13 +17,13 @@ const Mic = () => {
     <>
       <textarea value={speechText} onChange={(e)=>setSpeechText(e.target.value)} className="bg-transparent border w-5/6 h-24 mx-5 text-white" ></textarea>
       <p className="text-gray-300">{transcript}</p>
-      <div className="w-full flex flex-row justify-center items-center">
+      <div className="w-full flex flex-row justify-center items-center py-8 sm:py-0">
         {listening ?
-        <FaMicrophone className="text-red-500 text-2xl animate-pulse" onClick={SpeechRecognition.stopListening}>Stop</FaMicrophone> 
+        <FaMicrophone className="text-red-500 text-2xl animate-pulse cursor-pointer" onClick={SpeechRecognition.stopListening}>Stop</FaMicrophone> 
         : 
-        <FaMicrophone className="text-gray-400 text-2xl" onClick={()=>SpeechRecognition.startListening({ continuous: true })}>Start</FaMicrophone>
+        <FaMicrophone className="text-gray-400 text-2xl hover:text-red-500 cursor-pointer " onClick={()=>SpeechRecognition.startListening({ continuous: true })}>Start</FaMicrophone>
         }
-        <button className="text-gray-400 pl-4" onClick={resetTranscript}>Reset</button>
+        <GiTrashCan className="text-gray-400 text-center text-4xl font-light cursor-pointer transition-all ease-in-out   transform hover:rotate-90  hover:text-gray-600 pb-2 " onClick={resetTranscript}>Limpar texto</GiTrashCan>
       </div>
     </>    
   );
